9702337a0dd22ab81db7f9a4fcd0b31bdcaa78c5
UnLogic <johannocuaman@gmail.com>
2026-02-05 10:54:04 -0800

    Minimum Window Size
    Set minimum window size for app

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    minWidth: 600,
+    minHeight: 400,
     frame: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
